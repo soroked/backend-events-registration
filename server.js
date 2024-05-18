3c2ea9ffa252005b58f0ecf5ae933bf2a71a65e3
Eduard <sorokolietov@gmail.com>
2024-05-18 17:31:32 +0200

    add: local env for DB_HOST and PORT

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const {DB_HOST} = process.env
+const {DB_HOST, PORT} = process.env
 
 mongoose.set("strictQuery", true);
 
 mongoose.connect(DB_HOST)
-  .then(app.listen(3000))
+  .then(app.listen(PORT))
   .catch(error => {
     console.log(error.message);
     process.exit(1);
