629a0a1f2c8a6d7c7bbfad23ddb444e7f598148b
Eduard <sorokolietov@gmail.com>
2024-05-19 11:44:48 +0200

    add: validate body, when user registers

---
+++ middlewares/validateBody.js
@@ -0,0 +1,15 @@
+const { HttpError } = require('../helpers')
+
+const validateBody = (schema) => {
+  return (req, res, next) => {
+
+    const {error} = schema.validate(req.body, { abortEarly: false })
+
+    if (typeof error !== 'undefined') {
+      next(HttpError(400, error.message))
+    }
+    next();
+  }
+}
+
+module.exports = validateBody
--- routes/api/eventsRouter.js
+++ routes/api/eventsRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const ctrl = require("../../controllers/eventsController");
+const { userRegisterSchema } = require("../../models/user");
+const { validateBody } = require("../../middlewares");
 
 const router = express.Router();
 
@@ -8,6 +10,6 @@ router.get("/", ctrl.getEvents);
 
 router.get("/:id", ctrl.getEventByID);
 
-router.post("/:id/registration", ctrl.registerToEvent);
+router.post("/:id/registration", validateBody(userRegisterSchema), ctrl.registerToEvent);
 
 module.exports = router;
